cb5c1e39441ee58c6652c3de12b3d2295f207b6d
JosthinPaz <josthinpaz2@gmail.com>
2025-12-11 13:10:07 -0500

    Update database.py

--- BACKEND/db/database.py
+++ BACKEND/db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 # connection string
 # represenat la base de datos a conectar
 # dependiendo la base de datos que se use y el lenguaje de programación
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:admin@localhost:3315/josnishop"
+SQLALCHEMY_DATABASE_URL = os.environ.get("MYSQL_URL", "...")
 
 # crea el objeto de conexion (permite conectarse a la base de datos)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
